14de7096b7209804585650664c9be7b14426e8a2
Charankumar8297 <charankumar8297@gmail.com>
2025-05-13 20:11:37 +0530

    added

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -82,19 +82,19 @@ const User = require('../models/user.models');
         }
     };
     // Get user details by ID
-    const getUserDetails = async (req, res) => {
-        const userId = req.userId;
-        try {
-            const userDetails = await User.findById(userId);
-            if (!userDetails) {
-                return res.status(404).json({ message: 'User not found' });
-            }
-            return res.status(200).json({ message: 'User details fetched successfully', userDetails });
-        } catch (error) {
-            console.error('Error fetching user details:', error);
-            return res.status(500).json({ message: 'Internal server error' });
+const getUserDetails = async (req, res) => {
+    const userId = req.userId;
+    try {
+        const userDetails = await User.findById({ _id: userId })
+        if (!userDetails) {
+            return res.status(404).json({ message: 'User not found' });
         }
-    };
+        return res.status(200).json({ message: 'User details fetched successfully', userDetails });
+    } catch (error) {
+        console.error('Error fetching user details:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
 
     // Register user (update user details if not already registered)
     const registerUsers = async (req, res) => {
