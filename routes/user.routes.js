6074719862d320da5c9f9fd22c34901bc13e4231
Gnaneswar <gnaneswarkareti@gmail.com>
2025-06-12 12:55:47 +0530

    modified user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userAuth = require('../middlewares/userAuth')
 const {  getid,createUser,registerUsers, otpLogin, startRoute, verifyOtp, deleteUser, getUserDetails, emailOtp,updateUser,updateUserById  } = require('../controllers/user.controllers')
-
+const User=require('../models/user.models')
 //starter route
 router.get('/', startRoute)
 //otp login
@@ -20,6 +20,7 @@ router.delete('/delete-user/:id', deleteUser)
 //get user details 
 router.get('/user-details' ,userAuth,  getUserDetails)
 
+
 //send mail otp     
 router.post('/email-otp', emailOtp )
 
@@ -31,6 +32,14 @@ router.put('/updateAddress' , userAuth, )
 
 router.get('/getbyid/:id', getid)
 
-
+router.get('/users', async (req, res) => {
+    try {
+      const count = await User.countDocuments();
+      res.json({ count });
+    } catch (err) {
+      console.error('Error in /users:', err);
+      res.status(500).json({ error: 'Failed to fetch user count' });
+    }
+  });
 
 module.exports = router
